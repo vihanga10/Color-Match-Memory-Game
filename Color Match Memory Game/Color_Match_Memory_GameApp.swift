42f5ed8700fe68888437b57426467b0aabe388b5
vihanga10 <vihangamadushamini@gmail.com>
2026-01-17 10:17:23 +0530

    Initial Commit

---
+++ Color Match Memory Game/Color_Match_Memory_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Color_Match_Memory_GameApp.swift
+//  Color Match Memory Game
+//
+//  Created by Vihanga Madushamini on 2026-01-17.
+//
+
+import SwiftUI
+
+@main
+struct Color_Match_Memory_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
